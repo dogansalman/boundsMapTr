2d2b3b5c8d7b7619cc33aad91e44e70c5547984a
dogansalman <dogan.salman@yahoo.com>
2018-01-19 15:59:15 +0300

    update

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const port = process.env.PORT || 1331;
 const jsonfile = require('jsonfile');
 const fs = require('fs');
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', (request,response) => {
     response.send('bounds services');
 });
