8fa377d733510ec1d7f845056862fb6c4219cecf
dogansalman <dogan.salman@yahoo.com>
2018-01-19 10:47:51 +0300

    update

--- server.js
+++ server.js
@@ -10,8 +10,7 @@ app.get('/', (request,response) => {
 
 app.get('/:locId', (request, response) => {
     response.set('Content-Type', 'application/json');
-
-    jsonfile.readFile('data/bounds/city.json', (err,coords) => {
+    jsonfile.readFile('bounds/city.json', (err,coords) => {
         const _cord = coords.filter(c => c.id === Number(request.params.locId));
         if(_cord.length === 0) {
             response.statusCode = 404;  response.send({message: 'not found'});
@@ -19,6 +18,24 @@ app.get('/:locId', (request, response) => {
         }
         Object.assign(_cord[0], { multi_coords: Array.isArray(_cord[0].coordinates[0]) ? true : false });
         response.send(_cord[0]);
+    })
+});
+app.get('/:city_id/:town_id', (request, response) => {
+    response.set('Content-Type', 'application/json');
+
+    const _dir  = 'bounds/towns/' + request.params.city_id + '/' + request.params.town_id + '.json';
+
+    if (!fs.existsSync(_dir))  {
+        response.statusCode = 404;  response.send({message: 'not found'});
+    }
+
+
+    jsonfile.readFile(_dir, (err,coords) => {
+        //TODO kaynaktan düzeltilecek.
+        Object.assign(coords, {coordinates: coords.cordinates.coordinates});
+        delete coords.cordinates;
+        Object.assign(coords, { multi_coords: Array.isArray(coords.coordinates[0]) ? true : false });
+        response.send(coords);
     })
 });
 
